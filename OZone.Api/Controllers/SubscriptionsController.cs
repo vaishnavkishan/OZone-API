f512df823a598c60d00dd81335d9bec526aa24bd
tw-kishan-vaishnav <kishan.vaishnav@thoughtworks.com>
2023-09-16 16:27:41 +0530

    Accept email while creating subscription

--- OZone.Api/Controllers/SubscriptionsController.cs
+++ OZone.Api/Controllers/SubscriptionsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OZone.Api.Domain;
 using OZone.Api.Domain.Models;
 using OZone.Api.Models;
@@ -31,16 +32,33 @@ public class SubscriptionsController : ControllerBase
     /// <returns>Created subscription</returns>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    public CreateSubscriptionResponse Subscribe(Subscription subscription)
+    public async Task<IActionResult> Subscribe(CreateSubscriptionRequest req)
     {
-        var eventT = _db.Subscriptions.Add(subscription);
+        var user =await _db.Users.Where(x => x.Email == req.Email).FirstOrDefaultAsync();
+
+        if (user == null)
+        {
+           var entity= await _db.Users.AddAsync(new User { Name = req.Name, Email = req.Email });
+           user = entity.Entity;
+        }
+
+        if (await _db.Subscriptions.AnyAsync(x => x.UserId == user.Id && x.EventId == req.EventId))
+            return BadRequest("Already subscribed to the event!");
+
+        var sub = new Subscription
+        {
+            UserId = user.Id,
+            EventId = req.EventId
+        };
+        
+        var eventT = await _db.Subscriptions.AddAsync(sub);
         _db.SaveChanges();
 
-        return new CreateSubscriptionResponse
+        return Ok(new CreateSubscriptionResponse
         {
             Id = eventT.Entity.Id,
             EventId = eventT.Entity.EventId,
             UserId = eventT.Entity.UserId
-        };
+        });
     }
 }
--- OZone.Api/Models/CreateSubscriptionResponse.cs
+++ OZone.Api/Models/CreateSubscriptionResponse.cs
@@ -6,3 +6,10 @@ public class CreateSubscriptionResponse
     public Guid EventId { get; set; }
     public Guid UserId { get; set; }
 }
+
+public class CreateSubscriptionRequest
+{
+    public Guid EventId { get; set; }
+    public string Email { get; set; }= default!;
+    public string Name { get; set; } = default!;
+}
